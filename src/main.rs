1f8c9e9e97ab6ce132e782b156e93312b857af41
apinanyogaratnam <apinanapinan@icloud.com>
2022-05-14 22:04:02 -0400

    refactor: removed yew client applications

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,4 @@
-use yew::prelude::*;
 use serde::{Serialize, Deserialize};
-
-enum Msg {
-    AddOne,
-}
-
-struct Model {
-    value: i64,
-}
-
-impl Component for Model {
-    type Message = Msg;
-    type Properties = ();
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self {
-            value: 0,
-        }
-    }
-
-    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
-        match msg {
-            Msg::AddOne => {
-                self.value += 1;
-                // the value has changed so we need to
-                // re-render for it to appear on the page
-                true
-            }
-        }
-    }
-
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        // This gives us a component's "`Scope`" which allows us to send messages, etc to the component.
-        let link = ctx.link();
-        html! {
-            <div>
-                <button onclick={link.callback(|_| Msg::AddOne)}>{ "Add" }</button>
-                <p>{ self.value }</p>
-            </div>
-        }
-    }
-}
 
 #[derive(Debug, Serialize, Deserialize)]
 struct IP {
